f77b249e8d79089c3d5080d5d576e578f271d136
tshebo <120386361+tshebo@users.noreply.github.com>
2023-04-26 14:27:36 +0200

    Added a nethod that allows the user to enter their ingredients

--- Recipes/Ingredients.cs
+++ Recipes/Ingredients.cs
@@ -1,12 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
-namespace Recipes
+public class Recipe
 {
-    internal class Ingredients
+    // Fields
+    private string recipeName;
+    private int ingredientAmount;
+    private string[] ingredientNames;
+    private double[] ingredientQuantity;
+    private string[] ingredientUnits;
+    private int stepAmount;
+    private string[] stepDescriptions;
+    private double scaleFactor;
+
+    // Constructor
+    public Recipe(int ingredientAmount =1)
     {
+        this.ingredientAmount = ingredientAmount;
+        this.ingredientNames = new string[ingredientAmount];
+        this.ingredientQuantity = new double[ingredientAmount];
+        this.ingredientUnits = new string[ingredientAmount];
+    }
+
+    // Properties
+    public int IngredientAmount { get => ingredientAmount; set => ingredientAmount = value; }
+    public string[] IngredientNames { get => ingredientNames; set => ingredientNames = value; }
+    public double[] IngredientQuantity { get => ingredientQuantity; set => ingredientQuantity = value; }
+    public string[] IngredientUnits { get => ingredientUnits; set => ingredientUnits = value; }
+    public int StepAmount { get => stepAmount; set => stepAmount = value; }
+    public string[] StepDescriptions { get => stepDescriptions; set => stepDescriptions = value; }
+    public string RecipeName { get => recipeName; set => recipeName = value; }
+    public double ScaleFactor { get => scaleFactor; set => scaleFactor = value; }
+
+    public void getIngredients()
+    {
+        // Prompt for recipe name
+        Console.Write("Enter recipe name: \t\t\t\t");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        RecipeName = Console.ReadLine();
+        Console.ForegroundColor = ConsoleColor.White;
+
+        // Prompt for number of ingredients
+        
+        Console.Write("How many ingredients would you like to add? \t");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        while (!int.TryParse(Console.ReadLine(), out ingredientAmount) || ingredientAmount <= 0)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Invalid input. Please enter a positive integer.");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("How many ingredients would you like to add? \t");
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        IngredientAmount = ingredientAmount;
+
+        // Initialize arrays
+        IngredientNames = new string[IngredientAmount];
+        IngredientQuantity = new double[IngredientAmount];
+        IngredientUnits = new string[IngredientAmount];
+
+        // Store ingredients
+        for (int i = 0; i < IngredientAmount; i++)
+        {
+            Console.WriteLine($"==== Ingredient {i + 1} ====");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"Name of ingredient {i + 1}: \t\t\t\t");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            IngredientNames[i] = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            double quantity;
+            Console.Write($"Amount of {IngredientNames[i]}(s): \t\t\t\t");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            while (!double.TryParse(Console.ReadLine(), out quantity) || quantity <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Invalid input. Please enter a positive number.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"Amount of {IngredientNames[i]}(s):\t\t\t\t ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            IngredientQuantity[i] = quantity;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"Unit of measurement for {IngredientNames[i]}(s):\t\t");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            IngredientUnits[i] = Console.ReadLine();
+
+        }
     }
 }
--- Recipes/Program.cs
+++ Recipes/Program.cs
@@ -1,24 +1,25 @@
 ﻿internal class Program
 {
     static bool loop = true;
+    static Recipe recipe = new Recipe();
     
     private static void Main(string[] args)
     {
        while(loop)
         {
-            Menu();
+           validateChoice(Menu());
         } 
     }
     static string Menu()
     {
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine("=============================Welcome to the recipe app choose between the provided options==============================");
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("===== Welcome to the recipe app choose between the provided options =====");
         Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("\nEnter the number of the action you would like to perform");
         Console.WriteLine("(1) Add ingredients");
         Console.WriteLine("(2) Add steps");
         Console.WriteLine("(3) Display recipe ");
-        Console.WriteLine("(4) Scale Recipe")
+        Console.WriteLine("(4) Scale Recipe");
         Console.WriteLine("(5) Clear Data ");
         Console.WriteLine("Any key to Exit");
 
@@ -26,31 +27,52 @@
         return Console.ReadLine();
     }
 
-    static void validateChoice(string menu)
+    public static void validateChoice(string menu)
     {
         if (menu.Equals("1"))
         {
-
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("===== Add Ingredients =====");
+            Console.ForegroundColor = ConsoleColor.White;
+            recipe.getIngredients();
         }
         else if(menu.Equals("2"))
         {
-
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("===== Add Steps =====");
+            Console.ForegroundColor = ConsoleColor.White;
         }
         else if (menu.Equals("3"))
         {
-
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("===== Display Recipe =====");
+            Console.ForegroundColor = ConsoleColor.White;
         }
         else if (menu.Equals("4"))
         {
-
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("===== Scaling Options =====");
+            Console.ForegroundColor = ConsoleColor.White;
         }
         else if (menu.Equals("5"))
         {
-
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("===== Clear Data =====");
+            Console.ForegroundColor = ConsoleColor.White;
         }
         else{
-
+            Exit();
         }
+    }
+
+    public static bool Exit()
+    {
+        Console.ForegroundColor= ConsoleColor.DarkRed;
+        Console.WriteLine("Exiting the recipe app.");
+        Environment.Exit(0);
+        loop = false;
+        Console.ForegroundColor = ConsoleColor.White;
+        return loop;
     }
 
     
