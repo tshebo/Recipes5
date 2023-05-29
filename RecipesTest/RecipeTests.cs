9f402c319087c42019df02364bd6111d6e997c41
tshebo <120386361+tshebo@users.noreply.github.com>
2023-05-29 20:34:21 +0200

    Added the unit test

--- RecipesTest/RecipeTests.cs
+++ RecipesTest/RecipeTests.cs
@@ -6,6 +6,18 @@ namespace RecipesTest
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange
+            RecipeManager recipeManager = new RecipeManager();
+            Recipe recipe = new Recipe();
+            recipe.Calories.Add(100);
+            recipe.Calories.Add(150);
+            recipe.Calories.Add(200);
+
+            // Act
+            double totalCalories = recipeManager.CalculateTotalCalories(recipe);
+
+            // Assert
+            Assert.AreEqual(450, totalCalories);
         }
     }
 }
